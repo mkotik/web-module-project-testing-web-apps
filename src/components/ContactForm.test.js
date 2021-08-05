e124b21ba4b5c6d94aa6317b1863195bc406990b
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-08-05 15:08:33 -0400

    completed 5 tests

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-
 import ContactForm from "./ContactForm";
+
+const randNum = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
 test("renders without errors", () => {
   render(<ContactForm />);
@@ -16,11 +19,36 @@ test("renders the contact form header", () => {
   expect(header).toHaveTextContent("Contact Form");
 });
 
-test("renders ONE error message if user enters less then 5 characters into firstname.", async () => {});
+test("renders ONE error message if user enters less then 5 characters into firstname.", async () => {
+  render(<ContactForm />);
+  const inputText = "a";
+  const firstNameInput = screen.getByLabelText("First Name*");
+  userEvent.type(firstNameInput, inputText.repeat(randNum(1, 5)));
+  const errorMessages = screen.getAllByTestId("error");
+  expect(errorMessages).toHaveLength(1);
+  //   userEvent(firstNameInput).type("hel");
+  //   expect(errorMessages).toBeInTheDocument();
+});
 
-test("renders THREE error messages if user enters no values into any fields.", async () => {});
+test("renders THREE error messages if user enters no values into any fields.", async () => {
+  render(<ContactForm />);
+  const submitBtn = screen.getByRole("button");
+  userEvent.click(submitBtn);
+  const errorMessages = screen.getAllByTestId("error");
+  expect(errorMessages).toHaveLength(3);
+});
 
-test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {});
+test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {
+  render(<ContactForm />);
+  const firstNameInput = screen.getByLabelText("First Name*");
+  const lastNameInput = screen.getByLabelText("Last Name*");
+  const submitBtn = screen.getByRole("button");
+  userEvent.type(firstNameInput, "a".repeat(randNum(5, 15)));
+  userEvent.type(lastNameInput, "a".repeat(randNum(5, 15)));
+  userEvent.click(submitBtn);
+  const errorMessages = screen.getAllByTestId("error");
+  expect(errorMessages).toHaveLength(1);
+});
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {});
 
