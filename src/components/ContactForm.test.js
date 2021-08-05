39c5250031bfd629e65ae95b62a1c13f6f9a1d3a
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-08-05 15:48:14 -0400

    completed testing

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { getByTestId, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import ContactForm from "./ContactForm";
 
@@ -50,10 +50,72 @@ test("renders ONE error message if user enters a valid first name and last name 
   expect(errorMessages).toHaveLength(1);
 });
 
-test('renders "email must be a valid email address" if an invalid email is entered', async () => {});
+test('renders "email must be a valid email address" if an invalid email is entered', async () => {
+  render(<ContactForm />);
+  const emailInput = screen.getByLabelText("Email*");
+  userEvent.type(emailInput, "notrealemail");
+  const errorMessages = screen.getAllByTestId("error");
+  expect(errorMessages).toHaveLength(1);
+  expect(errorMessages[0]).toHaveTextContent(
+    "email must be a valid email address"
+  );
+});
 
-test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {});
+test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
+  render(<ContactForm />);
+  const firstNameInput = screen.getByLabelText("First Name*");
+  const emailInput = screen.getByLabelText("Email*");
+  const submitBtn = screen.getByRole("button");
+  userEvent.type(firstNameInput, "a".repeat(randNum(5, 15)));
+  userEvent.type(emailInput, "test@test.com");
+  userEvent.click(submitBtn);
+  const errorMessage = screen.getByTestId("error");
+  expect(errorMessage).toHaveTextContent("lastName is a required field");
+});
 
-test("renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.", async () => {});
+test("renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.", async () => {
+  render(<ContactForm />);
+  const firstNameInput = screen.getByLabelText("First Name*");
+  const lastNameInput = screen.getByLabelText("Last Name*");
+  const emailInput = screen.getByLabelText("Email*");
+  const submitBtn = screen.getByRole("button");
 
-test("renders all fields text when all fields are submitted.", async () => {});
+  userEvent.type(firstNameInput, "a".repeat(randNum(5, 15)));
+  userEvent.type(lastNameInput, "a".repeat(randNum(5, 15)));
+  userEvent.type(emailInput, "test@test.com");
+
+  let displayComponent = screen.queryByTestId("popup");
+  expect(displayComponent).not.toBeInTheDocument();
+
+  userEvent.click(submitBtn);
+
+  displayComponent = screen.queryByTestId("popup");
+  expect(displayComponent).toBeInTheDocument();
+  expect(displayComponent).toHaveTextContent(firstNameInput.value);
+  expect(displayComponent).toHaveTextContent(lastNameInput.value);
+  expect(displayComponent).toHaveTextContent(emailInput.value);
+});
+
+test("renders all fields text when all fields are submitted.", async () => {
+  render(<ContactForm />);
+  const firstNameInput = screen.getByLabelText("First Name*");
+  const lastNameInput = screen.getByLabelText("Last Name*");
+  const emailInput = screen.getByLabelText("Email*");
+  const messageInput = screen.getByLabelText("Message");
+
+  const submitBtn = screen.getByRole("button");
+
+  userEvent.type(firstNameInput, "a".repeat(randNum(5, 15)));
+  userEvent.type(lastNameInput, "a".repeat(randNum(5, 15)));
+  userEvent.type(emailInput, "test@test.com");
+  userEvent.type(messageInput, "This is a test message");
+
+  userEvent.click(submitBtn);
+
+  const displayComponent = screen.queryByTestId("popup");
+
+  expect(displayComponent).toHaveTextContent(firstNameInput.value);
+  expect(displayComponent).toHaveTextContent(lastNameInput.value);
+  expect(displayComponent).toHaveTextContent(emailInput.value);
+  expect(displayComponent).toHaveTextContent(messageInput.value);
+});
